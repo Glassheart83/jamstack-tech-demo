dca16dc39d5b63c9d0c90827da0e94bb90c59852
Samuele Farfarini <samuele.farfarini@gmail.com>
2020-05-22 21:58:50 +0200

    moved jekyll sources out of src directory leaving it just for webpack

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -3,11 +3,11 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: {
-        home: './src/_js/index.js'
+        home: './src/index.js'
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve('./src/assets'),
-        publicPath: '/src/assets/',
+        path: path.resolve('./assets'),
+        publicPath: '/assets/',
     }
 };
