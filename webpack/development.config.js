9517b44eb74a4437c0e67fc0e1f85e4b5a2a3d37
Samuele Farfarini <samuele.farfarini@gmail.com>
2020-06-07 18:01:08 +0200

    fixed imports with webpack and added remote font-awesome

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -1,10 +1,35 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     entry: {
         home: './src/js/index.js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/templates/default.html',
+            filename: '../_layouts/default.html',
+        }),
+        new FaviconsWebpackPlugin({
+            logo: './src/images/logo.png',
+            inject: true,
+            mode: 'webapp', // optional can be 'webapp' or 'light' - 'webapp' by default
+            devMode: 'webapp', // optional can be 'webapp' or 'light' - 'light' by default 
+            favicons: {
+                appName: 'jamstack-tech-demo',
+                appDescription: 'JAMStack Tech Demo',
+                background: '#ddd',
+                theme_color: '#333',
+                icons: {
+                    coast: false,
+                    yandex: false,
+                },
+            },
+        }),
+    ],
     module: {
         rules: [
             {
@@ -24,12 +49,21 @@ module.exports = {
                             },
                         },
                     },
-                    'sass-loader', // compiles Sass to CSS
+                    'sass-loader',
                 ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader'],
             },
         ],
     },
     output: {
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/dist/',
     },
 };
